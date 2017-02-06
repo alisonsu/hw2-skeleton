c1cc70eccfa3f03af249c2d575ee6d0a86682c33
Alison Su <alison.nc.su@gmail.com>
2017-02-05 21:26:55 -0800

    updated tests and included simple tests for partition and hierarchical clustering

--- test/test_cluster.py
+++ test/test_cluster.py
@@ -1,6 +1,10 @@
 from hw2skeleton import cluster
 from hw2skeleton import io
 import os
+import numpy as np
+import math
+import scipy.spatial.distance as ssd
+import scipy.cluster.hierarchy as hac
 
 def test_similarity():
     filename_a = os.path.join("data", "276.pdb")
@@ -10,7 +14,8 @@ def test_similarity():
     activesite_b = io.read_active_site(filename_b)
 
     # update this assertion
-    assert cluster.compute_similarity(activesite_a, activesite_b) == 0.0
+    x, y = cluster.compute_similarity(activesite_a, activesite_b)
+    assert int(y) == 106
 
 def test_partition_clustering():
     # tractable subset
@@ -20,9 +25,37 @@ def test_partition_clustering():
     for id in pdb_ids:
         filepath = os.path.join("data", "%i.pdb"%id)
         active_sites.append(io.read_active_site(filepath))
+    
+    M, C = cluster.cluster_by_partitioning(active_sites,3)
+    
+    labels = np.zeros(3, dtype=np.int64)
+    for c, value in C.items():
+        labels[value] = (c)
 
+    answer = np.array([0, 1, 2])
+    
     # update this assertion
-    assert cluster.cluster_by_partitioning(active_sites) == []
+    assert np.all(labels==answer)
+  
+def test_partition_clustering_simple():
+
+
+    X = np.array([[0,0,0], [0,1,0], [0,0,1], [100,100,100], [100,110,100]])
+    matrix = np.zeros([len(X), len(X)])    
+    for i in range(len(X)):
+        for j in range(len(X)):
+            matrix[i,j] = math.sqrt(math.pow(X[i][0]-X[j][0],2)+math.pow(X[i][1]-X[j][1],2)+math.pow(X[i][2]-X[j][2],2))
+    M, C = cluster.kMedoids(matrix,2)
+    
+    labels = np.zeros(len(X), dtype=np.int64)
+    for c, value in C.items():
+        labels[value] = (c)
+
+    
+    answer = np.array([0, 0, 0, 1, 1])
+        
+    # update this assertion
+    assert np.all(labels == answer)
 
 def test_hierarchical_clustering():
     # tractable subset
@@ -34,4 +67,27 @@ def test_hierarchical_clustering():
         active_sites.append(io.read_active_site(filepath))
 
     # update this assertion
-    assert cluster.cluster_hierarchically(active_sites) == []
+    # assert cluster.cluster_hierarchically(active_sites,3) == []
+   
+def test_hierarchical_clustering_simple():
+
+
+    X = np.array([[0,0,0], [0,1,0], [0,0,1], [100,100,100], [100,110,100]])
+    matrix = np.zeros([len(X), len(X)])    
+    for i in range(len(X)):
+        for j in range(len(X)):
+            matrix[i,j] = math.sqrt(math.pow(X[i][0]-X[j][0],2)+math.pow(X[i][1]-X[j][1],2)+math.pow(X[i][2]-X[j][2],2))
+    
+    distArray = ssd.squareform(matrix)
+    
+    z_centroid = hac.centroid(distArray)
+    cuttree = hac.cut_tree(z_centroid, n_clusters = 2)
+    labels_cuttree = []
+    for value in cuttree:
+        labels_cuttree.append(int(value))
+    
+    answer = np.array([0, 0, 0, 1, 1])
+        
+    # update this assertion
+    assert np.all(labels_cuttree == answer)
+
